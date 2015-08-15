63654772cba24182d7dd1832d6bdf6d109ea85e9
sam orlando <sam.orlando91@yahoo.com>
2015-08-15 00:44:57 -0400

    Add TE and PK
    also add function to e2e

--- src/dynasty_league/core.clj
+++ src/dynasty_league/core.clj
@@ -1,5 +1,6 @@
 (ns dynasty-league.core
-  (:require [dk.ative.docjure.spreadsheet :as xls]))
+  (:require [dk.ative.docjure.spreadsheet :as xls]
+            [dynasty-league.calculations :as calc]))
 
 (declare all-athletes)
 
@@ -14,7 +15,7 @@
        (rest)))
 
 (defmulti clean-athlete-data
-  "Filter extracted spreadsheet data."
+  "Extract and filter athlete data from excel spreadsheets."
   (fn [file codec] file))
 
 ;;; QBs
@@ -42,7 +43,7 @@
 
 (def rb-codec
   {:A :init-rank :B :name :C :team :D :bye-week :E :sos :F :gp-2014
-   :G :exp :H :atts :I :rush-yds :J :avg-rush :K :rush-td :L :fumbs
+   :G :exp :H :atts :I :rush-yds :J :avg-rush :K :rush-tds :L :fumbs
    :M :targets :N :recs :O :rec-yds :P :rec-tds :Q :return-yds :R :return-tds})
 
 (defn rb-threshold
@@ -63,17 +64,17 @@
 
 (def wr-codec
   {:A :init-rank :B :name :C :team :D :bye-week :E :sos
-   :F :gp-2014 :G :exp :H :targets :I :recs :J :rec-yds :K :rec-td
+   :F :gp-2014 :G :exp :H :targets :I :recs :J :rec-yds :K :rec-tds
    :L :return-yds :M :return-tds :N :qb-rank})
 
 (defn wr-threshold
   "Create tiers for wr based on :init-rank."[wrs]
-        (map #(cond (<= (:init-rank %) 11) (assoc % :worth 2)
-                    (<= (:init-rank %) 20) (assoc % :worth 3)
-                    (<= (:init-rank %) 40) (assoc % :worth 4)
-                    (<= (:init-rank %) 60) (assoc % :worth 5)
-                    (<= (:init-rank %) 80) (assoc % :worth 6)
-                    :else (assoc % :worth 7)) wrs))
+        (map #(cond (<= (:init-rank %) 11) (assoc % :worth 1)
+                    (<= (:init-rank %) 20) (assoc % :worth 2)
+                    (<= (:init-rank %) 40) (assoc % :worth 3)
+                    (<= (:init-rank %) 60) (assoc % :worth 4)
+                    (<= (:init-rank %) 80) (assoc % :worth 5)
+                    :else (assoc % :worth 6)) wrs))
 
 (defmethod clean-athlete-data "WR" [file codec]
   (as-> (load-spreadsheet file codec) $
@@ -83,9 +84,40 @@
 
 ;;; TEs
 
-;;; PNs
+(def te-codec
+  {:A :init-rank :B :name :D :team :E :adp :F :bye-week
+   :G :recs :H :yds :I :tds :M :fumbs :N :proj-pts
+   :O :price})
 
-;;; Ks
+(defn te-threshold
+  "Create tiers for te based on :init-rank."[tes]
+        (map #(cond (<= (:init-rank %) 4) (assoc % :worth 2)
+                    (<= (:init-rank %) 10) (assoc % :worth 3)
+                    (<= (:init-rank %) 25) (assoc % :worth 4)
+                    (<= (:init-rank %) 40) (assoc % :worth 5)
+                    :else (assoc % :worth 6)) tes))
+
+(defmethod clean-athlete-data "TE" [file codec]
+  (as-> (load-spreadsheet file codec) $
+        (map #(assoc % :position file) $)
+        (te-threshold $)))
+
+;;; PKs
+
+(def pk-codec
+  {:A :init-rank :B :name :C :team :D :bye-week
+   :E :fga :F :fgm :G :fgp :H :epa :I :exm :J :proj-pts})
+
+(defn pk-threshold
+  "Create tiers for pk based on :init-rank."[pks]
+        (map #(if (<= (:init-rank %) 20) (assoc % :worth 8) (assoc % :worth 9)) pks))
+
+(defmethod clean-athlete-data "PK" [file codec]
+  (as-> (load-spreadsheet file codec) $
+        (map #(assoc % :position file) $)
+        (pk-threshold $)))
+
+;;; PNs
 
 ;;; DEFs
 
@@ -94,9 +126,10 @@
 (def master-athlete-codec
   {"QB" qb-codec
    "RB" rb-codec
-   "WR" wr-codec})
+   "WR" wr-codec
+   "TE" te-codec
+   "PK" pk-codec})
 
-;; TODO: Put sequence of maps into one map with [:name :team] as vector key.
 (defn create-all-athlete-data
   "Extract all athlete files and combine into one datastructure. Takes
   in a map with filename-codec key-value pairs."
@@ -104,3 +137,9 @@
   (def all-athletes
     (flatten (for [[file codec] file-codec]
                (clean-athlete-data file codec)))))
+
+(defn full-process []
+  "Runs the process e2e and produces the best player at the end."
+  (do
+   (create-all-athlete-data master-athlete-codec)
+   (calc/calculate-best-player)))
